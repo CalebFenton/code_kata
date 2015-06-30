7b77e8d8d576fe5d187f0e69fefed052ebc36b09
Caleb Fenton <calebjfenton@gmail.com>
2015-06-30 16:16:37 +0700

    Add bloom filter days 3-5

--- bloom_filter/benchmark.rb
+++ bloom_filter/benchmark.rb
@@ -1,24 +1,25 @@
 require_relative 'lib/bloom_filter'
 
 # Add lots of random strings
-length = 10
-strings = []
-100.times { strings << rand(36 ** length).to_s(36) }
+length = lambda { 1 + rand(100) }
+count = 5_000
+strings = Array.new(count)
+count.times { |e| strings[e] = rand(36 ** length.call).to_s(36) }
 
-b = BloomFilter.new(8)
-strings.each { |s| b.add s }
+b = BloomFilter.new(count * 16)
+strings.each { |s| b << s }
 
 # Make sure they're all possibly included
-strings.each { |s| puts "#{s} should be included but isn't" unless b.include? s }
+strings.each { |s| puts "#{s} should be included but isn't" unless b.test s }
 
 # Rough check of false positives
 other_strings = []
-100.times do
-    string = rand(36 ** (1 + rand(100))).to_s(36)
+count.times do
+    string = rand(36 ** length.call).to_s(36)
     next if strings.include? string
     other_strings << string
 end
 
-fps = 0.0
-other_strings.each { |s| fps += 1.0 if b.include? s }
-puts "False positive %: #{100 * (fps / other_strings.size)}"
+fps = 0
+other_strings.each { |s| fps += 1 if b.test s }
+puts "False positive %: #{100 * (fps.to_f / other_strings.size)}"
--- bloom_filter/lib/bloom_filter.rb
+++ bloom_filter/lib/bloom_filter.rb
@@ -1,15 +1,18 @@
+require 'digest/sha1'
+require 'digest/md5'
+
 class BloomFilter
 
-    def initialize(bits = 16)
-        @bits = bits
-        @bitset = Array.new(2 ** @bits, 0)
+    def initialize(size = 100)
+        @bitset = Array.new(size, 0)
     end
 
     def add(s)
         hash(s).each { |h| @bitset[h] = 1 }
     end
+    alias :<< :add
 
-    def include?(s)
+    def test(s)
         hash(s).each { |h| return false unless @bitset[h] == 1 }
         true
     end
@@ -17,9 +20,11 @@ class BloomFilter
 private
 
     def hash(s)
-        h1 = s.hash % @bitset.size
-        h2 = s.sum(@bits)
-        [h1, h2]
+        [
+            s.hash % @bitset.size,
+            Digest::MD5.digest(s).hash % @bitset.size,
+            Digest::SHA1.digest(s).hash % @bitset.size,
+        ]
     end
 
 end
