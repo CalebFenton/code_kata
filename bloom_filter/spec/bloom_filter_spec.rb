326cf17518795cdcfeec7781cb5e9219fb09eec2
Caleb Fenton <calebjfenton@gmail.com>
2015-07-01 00:49:43 +0800

    Fix tests, whoops

--- bloom_filter/spec/bloom_filter_spec.rb
+++ bloom_filter/spec/bloom_filter_spec.rb
@@ -3,18 +3,18 @@ require 'bloom_filter'
 describe BloomFilter do
     let('bf') { BloomFilter.new }
 
-    describe '#include?' do
+    describe '#test' do
         it 'should include strings once added' do
             strings = []
             100.times { strings << rand(36 ** (1 + rand(100))).to_s(36) }
             strings.each { |s| bf.add s }
-            strings.each { |s| expect(bf.include?(s)).to be true }
+            strings.each { |s| expect(bf.test(s)).to be true }
         end
 
         it 'should not include strings that are not added' do
             strings = []
             100.times { strings << rand(36 ** (1 + rand(100))).to_s(36) }
-            strings.each { |s| expect(bf.include?(s)).to be false }
+            strings.each { |s| expect(bf.test(s)).to be false }
         end
     end
 
